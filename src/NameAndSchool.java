8de5c0c66c36461d0dc800ac4d4d17977bc289ac
karl.feng <Fengka@ssfs.org>
2021-09-07 10:38:35 -0400

    finished

---
+++ src/NameAndSchool.java
@@ -0,0 +1,6 @@
+public class NameAndSchool {
+    public static void main(String[] args) {
+        System.out.println("Karl Feng");
+        System.out.print("Sandy Spring Friends School");
+    }
+    }
---
+++ src/Sentence.java
@@ -0,0 +1,7 @@
+public class Sentence {
+    public static void main(String[] args){
+        System.out.println("It is a great");
+        System.out.println("time to be a");
+        System.out.println("computer programmer!");
+}
+    }
